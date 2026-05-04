13ba94e667df133cecac614dcc72efad0874cde0
tim apple <tim@tims-Air.attlocal.net>
2026-05-04 12:05:44 -0700

    sdf

---
+++ lua/config/mini.lua
@@ -0,0 +1,67 @@
+-- mini.hipatterns ------------------------------------------------------------
+local color_map
+
+local function enable_hipatterns(opts)
+    local hipatterns = require'mini.hipatterns'
+    hipatterns.enable(opts.buf, {
+        highlighters = {
+            hex_color = hipatterns.gen_highlighter.hex_color(),
+            named_color = {
+                pattern = '%w+',
+                group = function(_, match)
+                    local color = color_map[match]
+                    if color == nil then return nil end
+                    return hipatterns.compute_hex_color_group(color, 'bg')
+                end
+            },
+        },
+    })
+end
+
+local au = aug'my/hipatterns'
+au('BufEnter', {'papyrus.lua', 'rgb.txt', '*.css'}, function(opts)
+    -- Highlight hexadecimal colors
+    color_map = color_map or vim.tbl_map(
+        function(dec) return string.format('#%06x', dec) end,
+        vim.api.nvim_get_color_map()
+    )
+    enable_hipatterns(opts)
+end)
+
+-- Sourcing colorscheme calls `:hi clear`, which clears mini's highlight groups
+au('BufWritePost', 'papyrus.lua', function(opts)
+    package.loaded['mini.hipatterns'] = nil
+    enable_hipatterns(opts)
+end)
+
+-- mini.operators -------------------------------------------------------------
+require('mini.operators').setup({
+    evaluate = { prefix = 'g=' },
+    exchange = { prefix = 'cx' },
+    multiply = { prefix = 'gm' },
+    replace  = { prefix = 'gr' },
+    sort     = { prefix = 'gs' }
+})
+
+require('mini.operators').make_mappings(
+    'exchange',
+    { textobject = 'cx', line = 'cxx', selection = 'X' }
+)
+
+-- mini.diff ------------------------------------------------------------------
+require('mini.diff').setup({
+  view = {
+    style = vim.go.number and 'number' or 'sign',
+    signs = { add = '▒', change = '▒', delete = '▒' },
+    priority = 199,
+  },
+  mappings = {
+    apply = 'gh',
+    reset = 'gH',
+    textobject = 'gh',
+    goto_first = '[H',
+    goto_prev = '[h',
+    goto_next = ']h',
+    goto_last = ']H',
+  },
+})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,6 +8,7 @@ vim.pack.add({
     { src = gh'echasnovski/mini.operators',  version = 'stable' },
     { src = gh'echasnovski/mini.bufremove',  version = 'stable' },
     { src = gh'echasnovski/mini.hipatterns', version = 'stable' },
+    { src = gh'echasnovski/mini.diff',       version = 'stable' },
     { src = gh'tpope/vim-sleuth',            version = 'be69bff86754b1aa5adcbb527d7fcd1635a84080' },
     { src = gh'kylechui/nvim-surround',      version = '633a0ab03159569a66b65671b0ffb1a6aed6cf18' },
     { src = gh'AndrewRadev/linediff.vim',    version = '245d16328c47a132574e0fa4298d24a0f78b20b0' },
@@ -18,6 +19,7 @@ vim.pack.add({
 
 require_safe 'config.udir'
 require_safe 'config.ufind'
+require_safe 'config.mini'
 
 -- linediff -------------------------------------------------------------------
 vim.g.linediff_buffer_type = 'scratch'
@@ -27,47 +29,3 @@ map('x', 'D', "mode() is# 'V' ? ':Linediff<cr>' : 'D'", {expr = true})
 require'nvim-surround'.setup{
     indent_lines = false,
 }
-
--- mini.hipatterns ------------------------------------------------------------
-local color_map
-
-local function enable_hipatterns(opts)
-    local hipatterns = require'mini.hipatterns'
-    hipatterns.enable(opts.buf, {
-        highlighters = {
-            hex_color = hipatterns.gen_highlighter.hex_color(),
-            named_color = {
-                pattern = '%w+',
-                group = function(_, match)
-                    local color = color_map[match]
-                    if color == nil then return nil end
-                    return hipatterns.compute_hex_color_group(color, 'bg')
-                end
-            },
-        },
-    })
-end
-
-local au = aug'my/hipatterns'
-au('BufEnter', {'papyrus.lua', 'rgb.txt', '*.css'}, function(opts)
-    color_map = color_map or vim.tbl_map(
-        function(dec) return string.format('#%06x', dec) end,
-        vim.api.nvim_get_color_map()
-    )
-    enable_hipatterns(opts)
-end)
--- Sourcing colorscheme invokes `:hi clear`, which clears mini's highlight
--- groups.
-au('BufWritePost', 'papyrus.lua', function(opts)
-    package.loaded['mini.hipatterns'] = nil
-    enable_hipatterns(opts)
-end)
-
--- mini.operators -------------------------------------------------------------
-require('mini.operators').setup({
-    evaluate = { prefix = 'g=' },
-    exchange = { prefix = 'gx' },
-    multiply = { prefix = 'gm' },
-    replace  = { prefix = 'gr' },
-    sort     = { prefix = 'gs' }
-})
